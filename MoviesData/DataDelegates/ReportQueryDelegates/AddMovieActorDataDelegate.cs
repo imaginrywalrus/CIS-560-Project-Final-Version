968edb690e56d99b2da9aa9b3ca9d103bf108e16
James Myose <jmyose@live.com>
2021-05-04 20:23:21 -0500

    AddMovieActor

--- MoviesData/DataDelegates/ReportQueryDelegates/AddMovieActorDataDelegate.cs
+++ MoviesData/DataDelegates/ReportQueryDelegates/AddMovieActorDataDelegate.cs
@@ -6,7 +6,33 @@ using System.Data.SqlClient;
 
 namespace MoviesData.DataDelegates.ReportQueryDelegates
 {
-    class AddMovieActorDataDelegate
+    internal class AddMovieActorDataDelegate : DataReaderDelegate<MovieActor>
     {
+        private readonly double salary;
+
+        public AddMovieActorDataDelegate(float salary)
+           : base("Movies.AddMovieActor")
+        {
+            this.salary = salary;
+        }
+
+        public override void PrepareCommand(SqlCommand command)
+        {
+            base.PrepareCommand(command);
+
+            var p = command.Parameters.Add("Salary", SqlDbType.Float);
+            p.Value = salary;
+
+            p = command.Parameters.Add("ActorID", SqlDbType.Int);
+            p.Direction = ParameterDirection.Output;
+
+            p = command.Parameters.Add("MovieID", SqlDbType.Int);
+            p.Direction = ParameterDirection.Output;
+        }
+
+        public override MovieActor Translate(SqlCommand command, IDataRowReader reader)
+        {
+            return new MovieActor((int)command.Parameters["ActorID"].Value, (int)command.Parameters["MovieID"].Value, salary);
+        }
     }
 }
