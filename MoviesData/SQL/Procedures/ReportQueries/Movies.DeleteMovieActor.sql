5a20daa2f591b68a063edb556ba898a9f2449ee9
James Myose <jmyose@live.com>
2021-05-04 20:43:46 -0500

    simple deletes

---
+++ MoviesData/SQL/Procedures/ReportQueries/Movies.DeleteMovieActor.sql
@@ -0,0 +1,7 @@
+﻿CREATE OR ALTER PROCEDURE Movies.DeleteMovieActor
+    @ActorID INT,
+    @MovieID INT
+AS
+
+DELETE Movies.Review
+WHERE ActorID = @ActorID AND MovieID = @MovieID
---
+++ MoviesData/SQL/Procedures/ReportQueries/Movies.DeleteReview.sql
@@ -0,0 +1,6 @@
+﻿CREATE OR ALTER PROCEDURE Movies.DeleteReview
+    @ReviewID INT
+AS
+
+DELETE Movies.Review
+WHERE ReviewID = @ReviewID
