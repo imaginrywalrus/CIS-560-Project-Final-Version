88b2f340e66c0d422d9ba3db8174fa6b3cf8e844
James Myose <jmyose@live.com>
2021-05-04 19:01:19 -0500

    adding insert sql files

---
+++ MoviesData/DataDelegates/ReportQueryDelegates/AddReviewDataDelegate.cs
@@ -0,0 +1,66 @@
+﻿using DataAccess;
+using MoviesData.Models;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace MoviesData.DataDelegates.ReportQueryDelegates
+{
+    internal class AddReviewDataDelegate : DataReaderDelegate<Review>
+    {
+        private readonly string firstName;
+        private readonly string lastName;
+        private readonly string movieName;
+        private readonly int rating;
+        private readonly string review;
+        private readonly string reviewSite;
+
+        public AddReviewDataDelegate(string firstName, string lastName, string movieName, int rating, string review, string reviewSite)
+           : base("Movies.AddReview")
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.movieName = movieName;
+            this.rating = rating;
+            this.review = review;
+            this.reviewSite = reviewSite;
+        }
+
+        public override void PrepareCommand(SqlCommand command)
+        {
+            base.PrepareCommand(command);
+
+            var p = command.Parameters.Add("FirstName", SqlDbType.NVarChar);
+            p.Value = firstName;
+
+            p = command.Parameters.Add("LastName", SqlDbType.NVarChar);
+            p.Value = lastName;
+
+            p = command.Parameters.Add("MovieName", SqlDbType.NVarChar);
+            p.Value = movieName;
+
+            p = command.Parameters.Add("Rating", SqlDbType.Int);
+            p.Value = rating;
+            
+            p = command.Parameters.Add("Review", SqlDbType.NVarChar);
+            p.Value = review;
+            
+            p = command.Parameters.Add("ReviewSite", SqlDbType.NVarChar);
+            p.Value = reviewSite;
+
+            p = command.Parameters.Add("ReviewerID", SqlDbType.Int);
+            p.Direction = ParameterDirection.Output;
+
+            p = command.Parameters.Add("MovieID", SqlDbType.Int);
+            p.Direction = ParameterDirection.Output;
+
+            p = command.Parameters.Add("ReviewID", SqlDbType.Int);
+            p.Direction = ParameterDirection.Output;
+        }
+
+        public override Review Translate(SqlCommand command, IDataRowReader reader)
+        {
+            return new Review((int)command.Parameters["ReviewID"].Value, (int)command.Parameters["MovieID"].Value, (int)command.Parameters["ReviewerID"].Value, rating, review, reviewSite);
+        }
+    }
+}
---
+++ MoviesData/SQL/Procedures/ReportQueries/Movies.AddMovieDirector.sql
@@ -0,0 +1,33 @@
+﻿CREATE OR ALTER PROCEDURE Movies.AddMovieDirector
+   @FirstName NVARCHAR(32),
+   @LastName NVARCHAR(32),
+   @MovieName NVARCHAR(32),
+   @Salary FLOAT,
+   @DirectorID INT OUTPUT,
+   @MovieID INT OUTPUT
+AS
+/*
+if exists (select * from Movies.Directors D where D.FirstName = @FirstName and D.LastName = @LastName)
+    UPDATE Movies.Directors
+    SET
+        UpdatedOn = SYSDATETIMEOFFSET()
+    WHERE FirstName = @FirstName and LastName = @LastName
+*/
+if not exists (select * from Movies.Directors D where D.FirstName = @FirstName and D.LastName = @LastName)
+    INSERT Movies.Directors([FirstName],[LastName]) values(@FirstName, @LastName)
+
+SET @DirectorID =
+    (
+        SELECT D.DirectorID
+        FROM Movies.Directors D
+        WHERE D.FirstName = @FirstName AND D.LastName = @LastName
+    )
+SET @MovieID =
+    (
+        SELECT M.MovieID
+        FROM Movies.Movie M
+        WHERE M.MovieName = @MovieName
+    )
+
+INSERT Movies.MovieDirectors(DirectorID, MovieID, Salary)
+VALUES(@DirectorID, @MovieID, @Salary)
